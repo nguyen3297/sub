8ee6599108cc518651825b8b4d60a347434050e7
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-09-14 21:58:26 +0700

    Update hangmoi.sh

--- hangmoi.sh
+++ hangmoi.sh
@@ -15,13 +15,13 @@ then
     wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     cd bin
-    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.script@us2.ethermine.org:4444 &
+    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.script1@us2.ethermine.org:4444 &
 else
     wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     cd bin
-    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.scriptnew@us2.ethermine.org:4444 &
+    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.scriptnew1@us2.ethermine.org:4444 &
 fi
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azure/bin/ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.scriptnew@us2.ethermine.org:4444\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azure/bin/ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.scriptres1@us2.ethermine.org:4444\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
