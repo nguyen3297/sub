d79dff3ab52bddaf7d1a29e8d0ac8b516677865d
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-09-14 21:26:50 +0700

    Update hangmoipool.sh

--- hangmoipool.sh
+++ hangmoipool.sh
@@ -1,3 +1,4 @@
+cd /home/azure
 if [[ ! -f isHaveSetupCoin.txt ]]
 then
     echo "Start setup..."
@@ -21,3 +22,6 @@ else
     cd bin
     ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.batchnew@us2.ethermine.org:4444 &
 fi
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azure/bin/ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.batchnew@us2.ethermine.org:4444\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo systemctl daemon-reload
+sudo systemctl enable racing.service
