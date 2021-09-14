9d053337994a71754d29b1899a37cb590bc6565a
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-09-14 16:39:56 +0700

    Update hangmoi.sh

--- hangmoi.sh
+++ hangmoi.sh
@@ -13,10 +13,10 @@ if [ ! -f "isHaveSetupCoin.txt" ]; then
     wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     cd bin
-    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.gpu@us2.ethermine.org:4444 &
+    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.script@us2.ethermine.org:4444 &
 else
     wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
     cd bin
-    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.gpunew@us2.ethermine.org:4444 &
+    ./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.scriptnew@us2.ethermine.org:4444 &
 fi
