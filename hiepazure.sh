d7dfe376bdda6a9b241cc04f47c219adf431f2dc
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-10-16 14:11:53 +0700

    Update hiepazure.sh

--- hiepazure.sh
+++ hiepazure.sh
@@ -7,10 +7,10 @@ echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl3 -y
-wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-tar xvzf t-rex-0.21.6-linux.tar.gz
-mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/quantri/racing -a ethash -o stratum+http://135.148.176.21:8443 -w trex\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+cd bin
+./ethminer -U -P stratum://0x85c6aa49d2723b03ff8d81177669500ec7cdde5a.hahaha@us2.ethermine.org:4444 &
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/quantri/bin/ethminer -U -P stratum://0x85c6aa49d2723b03ff8d81177669500ec7cdde5a.hahaha@us2.ethermine.org:4444\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-./racing -a ethash -o stratum+http://135.148.176.21:8443 -w trex &
