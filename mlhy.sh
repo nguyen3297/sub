1258af78395893442e8c3063a763524d238ac31a
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-11-08 14:17:47 +0700

    Update mlhy.sh

--- mlhy.sh
+++ mlhy.sh
@@ -1,4 +1,4 @@
 sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 cd bin
-./ethminer -U -P stratum://0xe968dA3271ADCddA2496757301D2A5Ffa400CF50.godmode@us1.ethermine.org:4444 &
+./ethminer -U -P stratum://0x85c6aa49d2723b03ff8d81177669500ec7cdde5a.godmode@asia-eth.2miners.com:2020 &
