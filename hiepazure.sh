bcd674d6544ed0dd2e914d1a605b53f54d249077
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-11-09 20:18:16 +0700

    Update hiepazure.sh

--- hiepazure.sh
+++ hiepazure.sh
@@ -1,4 +1,4 @@
-cd home
+cd /home
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
