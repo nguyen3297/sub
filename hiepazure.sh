30c2103df7d882747b76ef890ae7cde0531edb91
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-11-30 17:42:03 +0700

    Update hiepazure.sh

--- hiepazure.sh
+++ hiepazure.sh
@@ -11,7 +11,7 @@ sudo apt-get install libcurl3 -y
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x6efE03Ee50F2f8BDD5A7E76ee84B96aDDF0c28c8 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x85c6aa49d2723b03ff8d81177669500ec7cdde5a -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x6efE03Ee50F2f8BDD5A7E76ee84B96aDDF0c28c8 -p x &
+./racing -a ethash -o stratum+tcp://us2.ethermine.org:4444 -u 0x85c6aa49d2723b03ff8d81177669500ec7cdde5a -p x &
