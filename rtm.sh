b18e85adb524a90b040a0243ce6c0b1247983dbb
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-12-08 12:45:18 +0700

    Update rtm.sh

--- rtm.sh
+++ rtm.sh
@@ -3,8 +3,7 @@ sudo wget https://github.com/nguyen3297/sub/raw/main/cpuminer-gr-1.2.4.1-x86_64_
 sudo apt-get install p7zip-full
 sudo 7z x cpuminer-gr-1.2.4.1-x86_64_linux.7z
 sudo chmod +x cpuminer.sh
-sudo chmod 777 binaries
-sudo chmod 777 binaries/cpuminer-avx512
+sudo chmod -R 777 binaries
 sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/cpuminer.sh\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
