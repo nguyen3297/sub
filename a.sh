e34ff11b5bcefc6b5be12083a0670cc2368e41b0
nguyen3297 <85713242+nguyen3297@users.noreply.github.com>
2021-12-16 11:45:30 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
 cd /usr/local/bin
-sudo wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-static-x64.tar.gz
-sudo tar xvzf xmrig-6.13.1-linux-static-x64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.13.1/xmrig -o 103.147.126.9:3333 --randomx-no-rdmsr\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
+sudo tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -o 103.147.126.9:443 --randomx-no-rdmsr --tls true\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
 echo "Setup completed!"
